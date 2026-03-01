944879d3a8d244b64daf7f88300b1297e657f0de
iSentrie <xvender7@gmail.com>
2026-03-01 09:24:30 +0200

    Add Lithuanian localization for sound settings

--- locales/lt.lua
+++ locales/lt.lua
@@ -1,1 +1,8 @@
-
+return {
+    ["soundConfigTitle"] = "Garso konfigūracija",
+    ["volumeLabel"] = "Garso lygis",
+    ["searchSounds"] = "Ieškoti garsų...",
+    ["noSoundsFound"] = "Garsų nerasta",
+    ["soundCount"] = "%s garsas(-ai)",
+    ["filterPresets"] = "Filtras",
+}
